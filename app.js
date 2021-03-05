6cdad53c68791e8fa9fba6a172bdcebea7348c44
Phoebe <phoebe.wilckens@gmail.com>
2021-03-05 01:17:19 -0300

    removed axios and added fetch

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
 function showInfo(response) {
-    console.log(response.data);
-    document.querySelector("#region").innerHTML = ` Region: ${response.data[0].region}`;
-    document.querySelector("#country").innerHTML = response.data[0].name;
-    document.querySelector("#subregion").innerHTML = response.data[0].subregion;
-    document.querySelector("#capital").innerHTML = response.data[0].capital;
+    console.log(response);
+    document.querySelector("#region").innerHTML = ` Region: ${response[0].region}`;
+    document.querySelector("#country").innerHTML = response[0].name;
+    document.querySelector("#subregion").innerHTML = response[0].subregion;
+    document.querySelector("#capital").innerHTML = response[0].capital;
     let limit = document.querySelector("#limitations");
-    let data = response.data[0].borders;
+    let data = response[0].borders;
     if (data.length > 0) {
         let title = document.createElement("p");
         title.appendChild(document.createTextNode("Borders:"));
         limit.append(title);
         let border = data.map(limits => {
-            axios.get(`https://restcountries.eu/rest/v2/alpha/${limits}`).then((country) => {
-                let countryName = country.data.name;
+            fetch(`https://restcountries.eu/rest/v2/alpha/${limits}`,{
+                method: 'GET'
+            }).then(response => response.json())
+            .then((country) => {
+                let countryName = country.name;
                 let li = document.createElement("li");
                 li.appendChild(document.createTextNode(countryName));
                 limit.append(li);
@@ -22,16 +25,23 @@ function showInfo(response) {
         limit.innerHTML = "No borders";
     }
 
-    document.querySelector("#language").innerHTML = response.data[0].languages[0].name;
-    let min = response.data[0].alpha3Code.toLowerCase();
+    document.querySelector("#language").innerHTML = response[0].languages[0].name;
+    let min = response[0].alpha3Code.toLowerCase();
     document.querySelector("#flag").setAttribute("src", `https://restcountries.eu/data/${min}.svg`);
-    document.querySelector("#currency-name").innerHTML = response.data[0].currencies[0].name;
-    document.querySelector("#currency-symbol").innerHTML = response.data[0].currencies[0].symbol;
+    document.querySelector("#currency-name").innerHTML = response[0].currencies[0].name;
+    document.querySelector("#currency-symbol").innerHTML = response[0].currencies[0].symbol;
 }
 
 function searchCountry(country) {
     let apiUrl = `https://restcountries.eu/rest/v2/name/${country}?fullText=true`;
-    axios.get(apiUrl).then(showInfo);
+    fetch(apiUrl,{
+        method: 'GET'
+    }).then(function(response) {
+        return response.json();
+    })
+    .then(function(myJson) {
+        showInfo(myJson);
+    });
 
 }
 
